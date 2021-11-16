98c9735ee7b388c5528579bb327f95d47a438c6a
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2021-11-16 18:44:23 -0300

    UserStatus ENUM Created

--- src/main/java/com/ead/authuser/models/UserModel.java
+++ src/main/java/com/ead/authuser/models/UserModel.java
@@ -1,2 +1,26 @@
-package com.ead.authuser.models;public class UserModel {
+package com.ead.authuser.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name="tb_users")
+public class UserModel implements Serializable {
+    private static final Long serialVersionUID = 1L;
+
+    private UUID userId;
+    private String username;
+    private String email;
+    private String password;
+    private String fullName;
+    private UserStatus userStatus;
+    private UserType userType;
+    private String phoneNumber;
+    private String cpf;
+    private String imageUrl;
+    private LocalDateTime creationDate;
+    private LocalDateTime lastUpdateDate;
 }
