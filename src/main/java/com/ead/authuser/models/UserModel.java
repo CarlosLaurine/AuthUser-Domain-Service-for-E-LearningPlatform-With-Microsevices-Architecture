746e5cd0188a91f9c01c2ac285f184ce66456421
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2021-11-16 19:07:16 -0300

    ORM Mapping + Lombok Data Annotation Implemented at UserModel

--- src/main/java/com/ead/authuser/models/UserModel.java
+++ src/main/java/com/ead/authuser/models/UserModel.java
@@ -1,28 +1,45 @@
 package com.ead.authuser.models;
 
 import com.ead.authuser.enums.UserStatus;
+import com.ead.authuser.enums.UserType;
+import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Data
 @Entity
 @Table(name="tb_users")
 public class UserModel implements Serializable {
     private static final Long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID userId;
+    @Column(nullable = false, unique = true, length = 50)
     private String username;
+    @Column(nullable = false, unique = true, length = 50)
     private String email;
+    @Column(nullable = false, length = 255)
     private String password;
+    @Column(nullable = false, length = 150)
     private String fullName;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private UserStatus userStatus;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private UserType userType;
+    @Column(length = 20)
     private String phoneNumber;
+    @Column(length = 20)
     private String cpf;
+    @Column
     private String imageUrl;
+    @Column(nullable = false)
     private LocalDateTime creationDate;
+    @Column(nullable = false)
     private LocalDateTime lastUpdateDate;
 }
