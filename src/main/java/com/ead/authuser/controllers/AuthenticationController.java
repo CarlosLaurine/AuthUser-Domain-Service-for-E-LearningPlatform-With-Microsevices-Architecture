8ab4230c57bb79127599dcb3d739548a995d67af
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2021-11-25 02:31:38 -0300

    JsonView RegistrationPost Implemented at AuthenticationController Method registerUser()

--- src/main/java/com/ead/authuser/controllers/AuthenticationController.java
+++ src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -5,6 +5,7 @@ import com.ead.authuser.enums.UserStatus;
 import com.ead.authuser.enums.UserType;
 import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserService;
+import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.BeanFactoryUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +24,8 @@ public class AuthenticationController {
     UserService userService;
 
     @PostMapping("/signup")
-    public ResponseEntity<Object> registerUser(@RequestBody UserDTO dto){
+    public ResponseEntity<Object> registerUser(@RequestBody
+                                                   @JsonView(UserDTO.UserView.RegistrationPost.class) UserDTO dto){
         if(userService.existsByUsername(dto.getUsername())) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Username already exists!");
         }
