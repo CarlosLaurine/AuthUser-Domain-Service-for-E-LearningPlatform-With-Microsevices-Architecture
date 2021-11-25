4b6331321b4542769c30ff5d60aee93f6d136838
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2021-11-25 02:30:34 -0300

    JsonView Implemented through UserView Interface at UserDTO for 4 different Views -  RegistrationPost, UserPut, PasswordPut and ImagePut

--- src/main/java/com/ead/authuser/dtos/UserDTO.java
+++ src/main/java/com/ead/authuser/dtos/UserDTO.java
@@ -1,6 +1,7 @@
 package com.ead.authuser.dtos;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Data;
 
 import java.util.UUID;
@@ -8,13 +9,29 @@ import java.util.UUID;
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserDTO {
+
+    public interface UserView {
+        interface RegistrationPost{}
+        interface UserPut{}
+        interface PasswordPut{}
+        interface ImagePut{}
+    }
+
     private UUID userId;
+    @JsonView(UserView.RegistrationPost.class)
     private String username;
+    @JsonView(UserView.RegistrationPost.class)
     private String email;
+    @JsonView({UserView.RegistrationPost.class, UserView.PasswordPut.class})
     private String password;
+    @JsonView(UserView.PasswordPut.class)
     private String oldPassword;
+    @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
     private String fullName;
+    @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
     private String phoneNumber;
+    @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
     private String cpf;
+    @JsonView(UserView.ImagePut.class)
     private String imageURL;
 }
