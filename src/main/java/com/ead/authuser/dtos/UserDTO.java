4fd5e0f602a94a975be77612ca643d4514174946
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2021-11-26 01:18:53 -0300

    Spring Validation Constraints Implemented at UserDTO according to the respective Business Rules applied to each JsonView

--- src/main/java/com/ead/authuser/dtos/UserDTO.java
+++ src/main/java/com/ead/authuser/dtos/UserDTO.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.UUID;
 
 @Data
@@ -18,12 +21,21 @@ public class UserDTO {
     }
 
     private UUID userId;
+
+    @NotBlank(groups = UserView.RegistrationPost.class)
+    @Size(min=4, max = 50)
     @JsonView(UserView.RegistrationPost.class)
     private String username;
+    @NotBlank(groups = UserView.RegistrationPost.class)
+    @Email
     @JsonView(UserView.RegistrationPost.class)
     private String email;
+    @NotBlank(groups = {UserView.RegistrationPost.class, UserView.PasswordPut.class})
+    @Size(min=6, max = 20)
     @JsonView({UserView.RegistrationPost.class, UserView.PasswordPut.class})
     private String password;
+    @NotBlank(groups = UserView.PasswordPut.class)
+    @Size(min=6, max = 20)
     @JsonView(UserView.PasswordPut.class)
     private String oldPassword;
     @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
@@ -32,6 +44,7 @@ public class UserDTO {
     private String phoneNumber;
     @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
     private String cpf;
+    @NotBlank(groups = UserView.ImagePut.class)
     @JsonView(UserView.ImagePut.class)
     private String imageURL;
 }
