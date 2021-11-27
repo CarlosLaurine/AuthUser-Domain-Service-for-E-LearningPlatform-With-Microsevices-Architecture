f5ecaf7ac5ab9a3c2efe62aba7cb9d7c92312fc1
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2021-11-27 15:42:44 -0300

    UserDTO Updated with @UsernameConstraint Annotation Implemented at username Attribute

--- src/main/java/com/ead/authuser/dtos/UserDTO.java
+++ src/main/java/com/ead/authuser/dtos/UserDTO.java
@@ -1,5 +1,6 @@
 package com.ead.authuser.dtos;
 
+import com.ead.authuser.validation.UsernameConstraint;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Data;
@@ -23,19 +24,20 @@ public class UserDTO {
     private UUID userId;
 
     @NotBlank(groups = UserView.RegistrationPost.class)
-    @Size(min=4, max = 50)
+    @Size(min=4, max = 50, groups = UserView.RegistrationPost.class)
+    @UsernameConstraint(groups = UserView.RegistrationPost.class)
     @JsonView(UserView.RegistrationPost.class)
     private String username;
     @NotBlank(groups = UserView.RegistrationPost.class)
-    @Email
+    @Email(groups = UserView.RegistrationPost.class)
     @JsonView(UserView.RegistrationPost.class)
     private String email;
     @NotBlank(groups = {UserView.RegistrationPost.class, UserView.PasswordPut.class})
-    @Size(min=6, max = 20)
+    @Size(min=6, max = 20, groups = {UserView.RegistrationPost.class, UserView.PasswordPut.class})
     @JsonView({UserView.RegistrationPost.class, UserView.PasswordPut.class})
     private String password;
     @NotBlank(groups = UserView.PasswordPut.class)
-    @Size(min=6, max = 20)
+    @Size(min=6, max = 20, groups = UserView.PasswordPut.class)
     @JsonView(UserView.PasswordPut.class)
     private String oldPassword;
     @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
