636c754e73462ec78a7c1e9f069572293db98f8e
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2021-11-29 03:05:39 -0300

    findAll() Method with Pagination parameter created at UserService Interface

--- src/main/java/com/ead/authuser/services/UserService.java
+++ src/main/java/com/ead/authuser/services/UserService.java
@@ -1,6 +1,8 @@
 package com.ead.authuser.services;
 
 import com.ead.authuser.models.UserModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,4 +15,6 @@ public interface UserService {
     void save(UserModel model);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
+
+    Page<UserModel> findAll(Pageable pageable);
 }
