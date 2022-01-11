88cd84b6d6ca55a2887c25878a3311a350cdbf8e
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2022-01-11 00:43:15 -0300

    JSON Pattern ISO 8601 UTC Implemented at UserModel Date Attributes

--- src/main/java/com/ead/authuser/models/UserModel.java
+++ src/main/java/com/ead/authuser/models/UserModel.java
@@ -44,10 +44,10 @@ public class UserModel extends RepresentationModel<UserModel> implements Seriali
     private String cpf;
     @Column
     private String imageUrl;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     @Column(nullable = false)
     private LocalDateTime creationDate;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     @Column(nullable = false)
     private LocalDateTime lastUpdateDate;
 }
