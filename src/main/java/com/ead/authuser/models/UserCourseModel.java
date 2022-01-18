9ef8678abd708930e4e17a3f99f7143bf8fd0782
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2022-01-17 22:18:13 -0300

    UserCourseModel Pivot Table Created + UserModel Cofigured with OneToMany Relationship

---
+++ src/main/java/com/ead/authuser/models/UserCourseModel.java
@@ -0,0 +1,25 @@
+package com.ead.authuser.models;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import org.hibernate.annotations.Fetch;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Entity
+@Table(name = "tb_users_courses")
+public class UserCourseModel implements Serializable {
+    private static final Long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @Column(nullable = false)
+    private UUID courseId;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private UserModel user;
+}
