43404458aa608deb730587c4b76bdce71168b54b
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2022-01-20 23:49:47 -0300

    Request URI Generation Externalized with UtilsService Interface and Implementation

---
+++ src/main/java/com/ead/authuser/services/impl/UtilsServiceImpl.java
@@ -0,0 +1,19 @@
+package com.ead.authuser.services.impl;
+
+import com.ead.authuser.services.UtilsService;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class UtilsServiceImpl implements UtilsService {
+
+    private String REQUEST_URI = "http://localhost:8082";
+
+    @Override
+    public String generateUrl(UUID userId, Pageable pageable) {
+        return REQUEST_URI + "/courses?userId=" + userId + "&page=" + pageable.getPageNumber() + "&size="
+                + pageable.getPageSize() + "&sort=" + pageable.getSort().toString().replaceAll(":", ",");
+    }
+}
