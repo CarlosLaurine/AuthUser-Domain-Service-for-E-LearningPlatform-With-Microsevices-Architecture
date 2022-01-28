68a632b8d7b09dbc01d097b54dd96b7cf80ea6d7
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2022-01-28 01:41:14 -0300

    Query Method existsByUserAndCourseId + Method save Configured at UserCourseService Interface + Implementation

--- src/main/java/com/ead/authuser/services/impl/UserCourseServiceImpl.java
+++ src/main/java/com/ead/authuser/services/impl/UserCourseServiceImpl.java
@@ -1,12 +1,26 @@
 package com.ead.authuser.services.impl;
 
+import com.ead.authuser.models.UserCourseModel;
+import com.ead.authuser.models.UserModel;
 import com.ead.authuser.repositories.UserCourseRepository;
 import com.ead.authuser.services.UserCourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public class UserCourseServiceImpl implements UserCourseService {
     @Autowired
     private UserCourseRepository userCourseRepository;
+
+    @Override
+    public boolean existsByUserAndCourseId(UserModel userModel, UUID courseId) {
+        return userCourseRepository.existsByUserAndCourseId(userModel, courseId);
+    }
+
+    @Override
+    public UserCourseModel save(UserCourseModel userCourseModel) {
+        return userCourseRepository.save(userCourseModel);
+    }
 }
