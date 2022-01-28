57648e1cc7aad4281beff32389162ab70950fe70
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2022-01-28 01:39:50 -0300

    Lombok Update -> AllArgsConstructor + AllArgsConstructor Added to UserCourseModel

--- src/main/java/com/ead/authuser/models/UserCourseModel.java
+++ src/main/java/com/ead/authuser/models/UserCourseModel.java
@@ -1,7 +1,9 @@
 package com.ead.authuser.models;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Fetch;
 
 import javax.persistence.*;
@@ -9,6 +11,8 @@ import java.io.Serializable;
 import java.util.UUID;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Entity
 @Table(name = "tb_users_courses")
