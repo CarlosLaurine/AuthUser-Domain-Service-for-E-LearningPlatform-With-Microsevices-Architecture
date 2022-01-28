55675854c3f482a499ba9d76c21e51aee42add81
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2022-01-28 01:42:44 -0300

    POST DB Update Method saveSubscriptionUserInCourse Configured and Implemented at UserCourseController

--- src/main/java/com/ead/authuser/controllers/UserCourseController.java
+++ src/main/java/com/ead/authuser/controllers/UserCourseController.java
@@ -2,6 +2,9 @@ package com.ead.authuser.controllers;
 
 import com.ead.authuser.clients.CourseClient;
 import com.ead.authuser.dtos.CourseDTO;
+import com.ead.authuser.dtos.UserCourseDTO;
+import com.ead.authuser.models.UserCourseModel;
+import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserCourseService;
 import com.ead.authuser.services.UserService;
 import lombok.extern.log4j.Log4j2;
@@ -13,6 +16,9 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
 import java.util.UUID;
 
 @Log4j2
@@ -21,18 +27,42 @@ import java.util.UUID;
 public class UserCourseController {
 
     @Autowired
-    private CourseClient courseClient;
-
-    @Autowired
     private UserService userService;
 
     @Autowired
     private UserCourseService userCourseService;
 
+    @Autowired
+    private CourseClient courseClient;
+
     @GetMapping("/users/{userId}/courses")
     public ResponseEntity<Page<CourseDTO>> getAllCoursesByUser(@PathVariable(value = "userId") UUID userId,
                                                                @PageableDefault(page = 0, size = 10, sort = "courseId", direction = Sort.Direction.ASC) Pageable pageable){
+
         return ResponseEntity.status(HttpStatus.OK).body(courseClient.getAllCoursesByUser(userId, pageable));
+
     }
+
+    @PostMapping("/users/{userId}/courses/subscription")
+    public ResponseEntity<Object> saveSubscriptionUserInCourse(@PathVariable(value = "userId") UUID userId,
+                                                               @RequestBody @Valid UserCourseDTO dto){
+
+        Optional<UserModel> userModelOptional = userService.findById(userId);
+
+        if(!userModelOptional.isPresent()){
+            return  ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
+        }
+
+        if(userCourseService.existsByUserAndCourseId(userModelOptional.get(), dto.getCourseId())){
+            return  ResponseEntity.status(HttpStatus.CONFLICT).body("User already registered for this Course");
+        }
+
+        UserCourseModel userCourseModel = userCourseService.save(userModelOptional.get().convertToUserCourseModel(dto.getCourseId()));
+
+        return  ResponseEntity.status(HttpStatus.CREATED).body(userCourseModel);
+    }
+
+
+
 
 }
