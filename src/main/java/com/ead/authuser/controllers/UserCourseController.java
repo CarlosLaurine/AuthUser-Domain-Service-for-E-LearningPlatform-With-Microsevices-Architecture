434ba2293996768b60069437d4253950db039bcd
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2022-02-13 02:54:10 -0300

    Delete Method deleteUserCourseByCourse Created and Configured at UserCourseController

--- src/main/java/com/ead/authuser/controllers/UserCourseController.java
+++ src/main/java/com/ead/authuser/controllers/UserCourseController.java
@@ -62,7 +62,14 @@ public class UserCourseController {
         return  ResponseEntity.status(HttpStatus.CREATED).body(userCourseModel);
     }
 
-
+    @DeleteMapping("/users/courses/{courseId}")
+    public ResponseEntity<Object> deleteUserCourseByCourse(@PathVariable(value = "courseId") UUID courseId){
+        if(!userCourseService.existsByCourseId(courseId)){
+            return  ResponseEntity.status(HttpStatus.NOT_FOUND).body("UserCourse not found.");
+        }
+        userCourseService.deleteUserCourseByCourseId(courseId);
+        return  ResponseEntity.status(HttpStatus.OK).body("UserCourse deleted successfully.");
+    }
 
 
 }
