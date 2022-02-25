a0463f8681fde4bf1b8a0d65c0215f33067b048d
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2022-02-24 22:42:26 -0300

    User and UserCourse Models Updated for Asynchronous Communication Implementation

--- src/main/java/com/ead/authuser/models/UserModel.java
+++ src/main/java/com/ead/authuser/models/UserModel.java
@@ -5,14 +5,12 @@ import com.ead.authuser.enums.UserType;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import org.springframework.hateoas.RepresentationModel;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
-import java.util.Set;
 import java.util.UUID;
 
 @Data
@@ -53,11 +51,4 @@ public class UserModel extends RepresentationModel<UserModel> implements Seriali
     @Column(nullable = false)
     private LocalDateTime lastUpdateDate;
 
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
-    private Set<UserCourseModel> userCourses;
-
-    public UserCourseModel convertToUserCourseModel(UUID courseId){
-        return new UserCourseModel(null, courseId, this);
-    }
 }
