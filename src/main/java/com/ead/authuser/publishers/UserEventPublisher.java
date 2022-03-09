ea53bdd87cf0d55611ea67ae7d43c6503f52ed82
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2022-03-08 22:14:33 -0300

    UserEventPublisher Component Created for Assync Communication

---
+++ src/main/java/com/ead/authuser/publishers/UserEventPublisher.java
@@ -0,0 +1,24 @@
+package com.ead.authuser.publishers;
+
+import com.ead.authuser.dtos.UserEventDTO;
+import com.ead.authuser.enums.ActionType;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserEventPublisher {
+
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+    @Value(value = "${ead.broker.exchange.user-event}")
+    private String exchangeUserEvent;
+
+    public void publishUserEvent(UserEventDTO userEventDTO, ActionType actionType) {
+        userEventDTO.setActionType(actionType.toString());
+        rabbitTemplate.convertAndSend(exchangeUserEvent, "", userEventDTO);
+    }
+
+}
