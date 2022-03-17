9491d2361670bdfb859b0fb7810450fabdf1d2dd
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2022-03-17 13:53:18 -0300

    Retry Structure with Fallback Method Configured at getAllCoursesByUser() Method

--- src/main/java/com/ead/authuser/clients/CourseClient.java
+++ src/main/java/com/ead/authuser/clients/CourseClient.java
@@ -3,6 +3,7 @@ package com.ead.authuser.clients;
 import com.ead.authuser.dtos.CourseDTO;
 import com.ead.authuser.dtos.ResponsePageDTO;
 import com.ead.authuser.services.UtilsService;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,6 +17,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -32,6 +34,7 @@ public class CourseClient {
     @Value("${ead.api.url.course}")
     private String REQUEST_URL_COURSE;
 
+    //@Retry(name = "default-retry-instance", fallbackMethod = "retryFallback")
     public Page<CourseDTO> getAllCoursesByUser(UUID userId, Pageable pageable){
         List<CourseDTO> searchResult = null;
         String requestUrl = REQUEST_URL_COURSE + utilsService.generateUrlGetAllCoursesByUser(userId, pageable);
@@ -46,6 +49,12 @@ public class CourseClient {
             log.error("Error request /courses endpoint exception {} ", exception);
         }
         log.info("Ending request /courses userId {} ", userId);
+        return new PageImpl<>(searchResult);
+    }
+
+    public Page<CourseDTO> retryFallback(UUID userId, Pageable pageable, Throwable exception){
+        log.error("Inside retryFallback Method, caused by -> {}", exception.toString());
+        List<CourseDTO> searchResult = new ArrayList<>();
         return new PageImpl<>(searchResult);
     }
 
