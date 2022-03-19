69029c681b3a4bc88d3e1c0ef21232fdac242f18
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2022-03-19 00:16:23 -0300

    CircuitBreaker with Default Instance default-circuitbreaker-instance Configured at getAllCoursesByUser() Method

--- src/main/java/com/ead/authuser/clients/CourseClient.java
+++ src/main/java/com/ead/authuser/clients/CourseClient.java
@@ -3,6 +3,7 @@ package com.ead.authuser.clients;
 import com.ead.authuser.dtos.CourseDTO;
 import com.ead.authuser.dtos.ResponsePageDTO;
 import com.ead.authuser.services.UtilsService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,6 +36,7 @@ public class CourseClient {
     private String REQUEST_URL_COURSE;
 
     //@Retry(name = "default-retry-instance", fallbackMethod = "retryFallback")
+    @CircuitBreaker(name = "default-circuitbreaker-instance")
     public Page<CourseDTO> getAllCoursesByUser(UUID userId, Pageable pageable){
         List<CourseDTO> searchResult = null;
         String requestUrl = REQUEST_URL_COURSE + utilsService.generateUrlGetAllCoursesByUser(userId, pageable);
