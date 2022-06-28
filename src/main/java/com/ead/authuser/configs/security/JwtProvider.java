9201d65f4a2653a8577526fa78dbc30089cfd7c7
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2022-06-28 01:41:49 -0300

    getUsernameJwt() and validateJwt() Methods Added to JwtProvider

--- src/main/java/com/ead/authuser/configs/security/JwtProvider.java
+++ src/main/java/com/ead/authuser/configs/security/JwtProvider.java
@@ -27,4 +27,29 @@ public class JwtProvider {
                 .signWith(SignatureAlgorithm.HS512, jwtSecret)
                 .compact();
     }
+
+    public String getUsernameJwt(String token) {
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateJwt(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            return true;
+        } catch (IllegalArgumentException e) {
+            log.error("JWT claims string is empty: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            log.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (MalformedJwtException e) {
+            log.error("Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            log.error("JWT token is expired: {}", e.getMessage());
+        } catch (SignatureException e) {
+            log.error("Invalid JWT signature: {}", e.getMessage());
+        }
+        return false;
+    }
+
+
+
 }
