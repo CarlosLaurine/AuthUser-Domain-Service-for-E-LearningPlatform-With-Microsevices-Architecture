ed8ed961a8c9bd225a19829e6c7c16393258ebb6
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2022-07-10 02:17:37 -0300

    Method findById() with EntityGraph Specified for Roles Collection Loading Configured at UserRepository

--- src/main/java/com/ead/authuser/repositories/UserRepository.java
+++ src/main/java/com/ead/authuser/repositories/UserRepository.java
@@ -8,8 +8,15 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<UserModel, UUID>, JpaSpecificationExecutor<UserModel> {
+
     boolean existsByUsername(String username);
+
     boolean existsByEmail(String email);
+
     @EntityGraph(attributePaths = "roles", type = EntityGraph.EntityGraphType.FETCH)
     Optional<UserModel> findByUsername(String username);
+
+    @EntityGraph(attributePaths = "roles", type = EntityGraph.EntityGraphType.FETCH)
+    Optional<UserModel> findById(UUID userId);
+
 }
