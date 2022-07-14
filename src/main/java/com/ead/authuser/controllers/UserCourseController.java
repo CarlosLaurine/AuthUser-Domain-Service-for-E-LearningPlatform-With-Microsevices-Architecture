96a2a0d94dc680a510e3e298d5f885616db0c01f
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2022-07-14 02:13:52 -0300

    Pre Authorization Set for STUDENT Role at getAllCoursesByUser

--- src/main/java/com/ead/authuser/controllers/UserCourseController.java
+++ src/main/java/com/ead/authuser/controllers/UserCourseController.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -26,11 +27,13 @@ public class UserCourseController {
     @Autowired
     private CourseClient courseClient;
 
+    @PreAuthorize("hasAnyRole('STUDENT')")
     @GetMapping("/users/{userId}/courses")
     public ResponseEntity<Page<CourseDTO>> getAllCoursesByUser(@PathVariable(value = "userId") UUID userId,
-                                                               @PageableDefault(page = 0, size = 10, sort = "courseId", direction = Sort.Direction.ASC) Pageable pageable){
+                                                               @PageableDefault(page = 0, size = 10, sort = "courseId", direction = Sort.Direction.ASC) Pageable pageable,
+                                                               @RequestHeader("Authorization") String authToken){
 
-        return ResponseEntity.status(HttpStatus.OK).body(courseClient.getAllCoursesByUser(userId, pageable));
+        return ResponseEntity.status(HttpStatus.OK).body(courseClient.getAllCoursesByUser(userId, pageable, authToken));
 
     }
 }
